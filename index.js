f2638d270aa1a50d83bce89af3f6536e1ca1936b
ShashankEC37 <jsr.shashank@gmail.com>
2023-05-09 16:05:37 +0530

    First Commitssss

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-  console.log('Connected to MongoDB database.');
+  console.log('Connected t MongoDB database.');
 
 });
 
